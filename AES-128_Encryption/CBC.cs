dd52557d3bbbcd421db24c2076dace3eacf846b5
Ertan Turan <ertanturan@outlook.com>
2020-01-23 11:05:27 +0300

    Test data added. CBC Trial coded.
    Note that Keyvalue and IV values are template.

---
+++ AES-128_Encryption/CBC.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AES_128_Encryption
+{
+    public class CBC
+    {
+        private string _keyValue;
+        private string _iVValue;
+
+        public CBC(string keyValue, string IVValue)
+        {
+            _keyValue = keyValue;
+            _iVValue = IVValue;
+        }
+        public string Encrypt(string encryptStr)
+        {
+            byte[] keyArray = UTF8Encoding.UTF8.GetBytes(_keyValue);
+            byte[] IVArray = UTF8Encoding.UTF8.GetBytes(_iVValue);
+
+            byte[] toEncryptArray = UTF8Encoding.UTF8.GetBytes(encryptStr);
+            byte[] resultArray;
+            using (Rijndael rDel = Rijndael.Create())
+            {
+                rDel.KeySize = 128;
+                rDel.BlockSize = 128;
+                rDel.Key = keyArray;
+                rDel.IV = IVArray;
+
+                rDel.Mode = CipherMode.CBC;
+
+                rDel.Padding = PaddingMode.PKCS7;
+
+
+                ICryptoTransform cTransform = rDel.CreateEncryptor();
+
+                resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+            }
+
+            //RijndaelManaged rDel = new RijndaelManaged();
+
+
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+
+        }
+
+        public string Decrypt(string decryptStr)
+        {
+
+            byte[] keyArray = UTF8Encoding.UTF8.GetBytes(_keyValue);
+            byte[] IVArray = UTF8Encoding.UTF8.GetBytes(_iVValue);
+
+            byte[] toEncryptArray = Convert.FromBase64String(decryptStr);
+
+            byte[] resultArray;
+
+            using (Rijndael rDel = Rijndael.Create())
+            {
+                rDel.KeySize = 128;
+                rDel.BlockSize = 128;
+                rDel.Key = keyArray;
+
+                rDel.IV = IVArray;
+
+                //rDel.KeySize = 128;
+
+                rDel.Mode = CipherMode.CBC;
+
+                rDel.Padding = PaddingMode.PKCS7;
+
+
+                ICryptoTransform cTransform = rDel.CreateDecryptor();
+                resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+            }
+
+            //RijndaelManaged rDel = new RijndaelManaged();
+
+
+
+
+
+            return UTF8Encoding.UTF8.GetString(resultArray);
+
+        }
+    }
+}
--- AES-128_Encryption/Program.cs
+++ AES-128_Encryption/Program.cs
@@ -1,111 +1,67 @@
 ﻿using System;
-using System.Security.Cryptography;
-using System.Text;
-
 namespace AES_128_Encryption
 {
     class AESTest
     {
         public static void Main()
         {
-            string original = "Here is some data to encrypt!";
+            string original = "Şifrelenmesi için türkçe karakter kullanım örneği : ğüşiöçı";
             //128 bit (16 byte) Key value here
-            string keyValue = " "; //128 bit (16 byte) Key value here
-            string IVValue = " ";//128 bit (16 byte) initial vector value here
+            string keyValue = "4582B07C2F9AE5E9"; //128 bit (16 byte) Key value here
+            string IVValue = "3C26D685F5A69E75";//128 bit (16 byte) initial vector value here
 
-            byte[] key = Encoding.ASCII.GetBytes(keyValue);
-            byte[] IV = Encoding.ASCII.GetBytes(IVValue);
+            CBC cbcbTrial = new CBC(keyValue, IVValue);
 
-            Console.WriteLine(IV.Length);
+            string encrypted = cbcbTrial.Encrypt(original);
+            string decrypted = cbcbTrial.Decrypt(encrypted);
+            Console.WriteLine("ORIGINAL : " + original);
+            Console.WriteLine("ENCRYPTED : " + encrypted);
+            Console.WriteLine("DECRYPTED : " + decrypted);
 
-            // Encrypted the string.
-            string encrypted = Encrypt(original, keyValue);
+            ////Display the original data and the decrypted data.
+            //Console.WriteLine("Original:   {0}", original);
+            ////Console.WriteLine("Encrypted :   {0}", encrypted);
+            ////Console.WriteLine("Round Trip: {0}", roundtrip);
 
-            // Decrypted string.
-            string roundtrip = Decrypt(encrypted, keyValue);
-
-            //Display the original data and the decrypted data.
-            Console.WriteLine("Original:   {0}", original);
-            Console.WriteLine("Encrypted :   {0}", encrypted);
-            Console.WriteLine("Round Trip: {0}", roundtrip);
+            //Console.WriteLine(" ");
+            //int b = 100 ^ 33;
+            //Console.WriteLine(GetIntBinaryString(100));
+            //Console.WriteLine(GetIntBinaryString(33));
+            //Console.WriteLine(GetIntBinaryString(b));
 
             Console.Read();
 
         }
 
-        public static string Encrypt(string encryptStr, string key)
+
+
+        static string GetIntBinaryString(int n)
         {
+            char[] b = new char[32];
+            int pos = 31;
 
-            byte[] keyArray = UTF8Encoding.UTF8.GetBytes(key);
-
-            byte[] toEncryptArray = UTF8Encoding.UTF8.GetBytes(encryptStr);
-            byte[] resultArray;
-            using (Rijndael rDel = Rijndael.Create())
+            for (int i = 0; i < 32; i++)
             {
-                rDel.KeySize = 128;
-                rDel.BlockSize = 128;
-                rDel.Key = keyArray;
-                rDel.IV = keyArray;
-
-                rDel.Mode = CipherMode.CBC;
-
-                rDel.Padding = PaddingMode.PKCS7;
-
-
-                ICryptoTransform cTransform = rDel.CreateEncryptor();
-
-                resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+                if ((n & (1 << i)) != 0)
+                {
+                    b[pos] = '1';
+                }
+                else
+                {
+                    b[pos] = '0';
+                }
+                pos--;
             }
 
-            //RijndaelManaged rDel = new RijndaelManaged();
-
-
-
-
-
-
-            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
-
+            return new string(b);
         }
 
-        public static string Decrypt(string decryptStr, string key)
+        private static string XOR()
         {
 
-            byte[] keyArray = UTF8Encoding.UTF8.GetBytes(key);
 
-            byte[] toEncryptArray = Convert.FromBase64String(decryptStr);
-
-            byte[] resultArray;
-
-            using (Rijndael rDel = Rijndael.Create())
-            {
-                rDel.KeySize = 128;
-                rDel.BlockSize = 128;
-                rDel.Key = keyArray;
-
-                rDel.IV = keyArray;
-
-                //rDel.KeySize = 128;
-
-                rDel.Mode = CipherMode.CBC;
-
-                rDel.Padding = PaddingMode.PKCS7;
-
-
-                ICryptoTransform cTransform = rDel.CreateDecryptor();
-                resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
-            }
-
-            //RijndaelManaged rDel = new RijndaelManaged();
-
-
-
-
-
-            return UTF8Encoding.UTF8.GetString(resultArray);
-
+            return " ";
         }
-
     }
 }
 
